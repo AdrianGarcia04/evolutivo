9f7cef19bb66424e9f16beabafab74cd4a4ab598
Adrián García Pérez <adrian.garcia04@ciencias.unam.mx>
2021-01-10 19:11:22 -0600

    T6: New population

--- t6/src/Instance.py
+++ t6/src/Instance.py
@@ -12,10 +12,11 @@ class Instance:
         self.scale = scale
         self.dim = dim
         self.ranges = ranges[funct]
-        self.funct = functs[funct]
+        self.eval = functs[funct]
         self.population = np.random.uniform(low=-self.ranges, high=self.ranges, size=(size, dim))
 
-    def generate_test_vect(self, i):
+    def gen_mut_vect(self, i):
+
         rands = np.random.randint(0, self.size, size=3)
         while len(set(list(rands) + [i])) != 4:
             rands = np.random.randint(0, self.size, size=3)
@@ -25,3 +26,17 @@ class Instance:
 
         diff = self.scale * (x_2 - x_3)
         return x_1 + diff
+
+    def gen_test_vec(self, v_i, x_i):
+
+        j_r = np.random.randint(0, self.dim)
+        u_i = np.zeros(self.dim)
+
+        for j in range(self.dim):
+            r_cj = np.random.rand()
+            if (r_cj < self.cross) or (j == j_r):
+                u_i[j] = v_i[j]
+            else:
+                u_i[j] = x_i[j]
+
+        return u_i
--- t6/src/main.py
+++ t6/src/main.py
@@ -13,7 +13,20 @@ def main(args):
     }
     instance = Instance(**kwargs)
 
-    for i in range(instance.maxiters):
-        v_i = instance.generate_test_vect(i)
+    for curr_i in range(instance.maxiters):
+        print(curr_i, end='\r')
+        test_vectors = []
+        for (i, x_i) in enumerate(instance.population):
+            v_i = instance.gen_mut_vect(i)
+
+            u_i = instance.gen_test_vec(v_i, x_i)
+            test_vectors.append(u_i)
+            if instance.eval(u_i) < instance.eval(x_i):
+                instance.population[i] = u_i
+
+    for ind in instance.population:
+        print(instance.eval(ind))
+
+
 
 main(arguments.defineArgs())
